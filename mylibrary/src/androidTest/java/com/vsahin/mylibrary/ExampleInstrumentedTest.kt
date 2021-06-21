b1ba81e2439a934455aac2b82617f1caeaff9a16
Volkan Şahin <me@vsahin.com>
2021-06-21 16:20:38 +0200

    Add library changing text and UI tests

--- mylibrary/src/androidTest/java/com/vsahin/mylibrary/ExampleInstrumentedTest.kt
+++ mylibrary/src/androidTest/java/com/vsahin/mylibrary/ExampleInstrumentedTest.kt
@@ -1,5 +1,10 @@
 package com.vsahin.mylibrary
 
+import androidx.test.espresso.Espresso
+import androidx.test.espresso.action.ViewActions
+import androidx.test.espresso.assertion.ViewAssertions
+import androidx.test.espresso.matcher.ViewMatchers
+import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
 
@@ -7,6 +12,8 @@ import org.junit.Test
 import org.junit.runner.RunWith
 
 import org.junit.Assert.*
+import org.junit.Before
+import org.junit.Rule
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -15,10 +22,31 @@ import org.junit.Assert.*
  */
 @RunWith(AndroidJUnit4::class)
 class ExampleInstrumentedTest {
+    private lateinit var stringToBetyped: String
+
+    @get:Rule
+    var activityRule: ActivityScenarioRule<MyLibraryActivity> =
+        ActivityScenarioRule(MyLibraryActivity::class.java)
+
+    @Before
+    fun initValidString() {
+        stringToBetyped = "Library"
+    }
+
     @Test
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
         assertEquals("com.vsahin.mylibrary", appContext.packageName)
+    }
+
+    @Test
+    fun changeText_sameActivity() {
+        Espresso.onView(ViewMatchers.withId(R.id.editText))
+            .perform(ViewActions.typeText(stringToBetyped), ViewActions.closeSoftKeyboard())
+        Espresso.onView(ViewMatchers.withId(R.id.button)).perform(ViewActions.click())
+
+        Espresso.onView(ViewMatchers.withId(R.id.textView))
+            .check(ViewAssertions.matches(ViewMatchers.withText(stringToBetyped)))
     }
 }
--- mylibrary/src/main/java/com/vsahin/mylibrary/MyLibraryActivity.kt
+++ mylibrary/src/main/java/com/vsahin/mylibrary/MyLibraryActivity.kt
@@ -2,10 +2,27 @@ package com.vsahin.mylibrary
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class MyLibraryActivity : AppCompatActivity() {
+    lateinit var editText: EditText
+    lateinit var textView: TextView
+    lateinit var button: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_my_library)
+
+        editText = findViewById(R.id.editText)
+        textView = findViewById(R.id.textView)
+        button = findViewById(R.id.button)
+
+        button.setOnClickListener {
+            textView.text = editText.text.toString()
+        }
     }
+
+    fun libraryUncoveredFunction() = 1
 }
