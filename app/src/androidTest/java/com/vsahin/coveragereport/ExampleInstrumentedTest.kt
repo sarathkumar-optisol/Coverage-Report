d4102d816a9535f019fe472aa485eaaecf17a55b
Volkan Şahin <me@vsahin.com>
2021-06-21 15:54:04 +0200

    Add changing text and UI tests

--- app/src/androidTest/java/com/vsahin/coveragereport/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/vsahin/coveragereport/ExampleInstrumentedTest.kt
@@ -1,5 +1,11 @@
 package com.vsahin.coveragereport
 
+import androidx.test.espresso.Espresso
+import androidx.test.espresso.action.ViewActions
+import androidx.test.espresso.assertion.ViewAssertions
+import androidx.test.espresso.matcher.ViewMatchers
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
 
@@ -7,6 +13,8 @@ import org.junit.Test
 import org.junit.runner.RunWith
 
 import org.junit.Assert.*
+import org.junit.Before
+import org.junit.Rule
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -15,10 +23,31 @@ import org.junit.Assert.*
  */
 @RunWith(AndroidJUnit4::class)
 class ExampleInstrumentedTest {
+    private lateinit var stringToBetyped: String
+
+    @get:Rule
+    var activityRule: ActivityScenarioRule<MainActivity> =
+        ActivityScenarioRule(MainActivity::class.java)
+
+    @Before
+    fun initValidString() {
+        stringToBetyped = "Espresso"
+    }
+
     @Test
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
         assertEquals("com.vsahin.coveragereport", appContext.packageName)
+    }
+
+    @Test
+    fun changeText_sameActivity() {
+        Espresso.onView(withId(R.id.editText))
+            .perform(ViewActions.typeText(stringToBetyped), ViewActions.closeSoftKeyboard())
+        Espresso.onView(withId(R.id.button)).perform(ViewActions.click())
+
+        Espresso.onView(withId(R.id.textView))
+            .check(ViewAssertions.matches(ViewMatchers.withText(stringToBetyped)))
     }
 }
--- app/src/main/java/com/vsahin/coveragereport/MainActivity.kt
+++ app/src/main/java/com/vsahin/coveragereport/MainActivity.kt
@@ -1,11 +1,28 @@
 package com.vsahin.coveragereport
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+    lateinit var editText: EditText
+    lateinit var textView: TextView
+    lateinit var button: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        editText = findViewById(R.id.editText)
+        textView = findViewById(R.id.textView)
+        button = findViewById(R.id.button)
+
+        button.setOnClickListener {
+            textView.text = editText.text.toString()
+        }
     }
+
+    fun uncoveredFunction() = 1
 }
